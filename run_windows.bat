9a464e30d5e322ab5d3b0fb97ff258185dc0a1b9
Tom Pinargote <tpinargote@fi.uba.ar>
2026-06-04 14:18:42 -0300

    Update run_windows.bat to automatically open the UI

--- run_windows.bat
+++ run_windows.bat
@@ -4,6 +4,9 @@ echo Inciando Servidor del Agente de Histologia...
 echo ==============================================
 echo.
 
+echo Abriendo la interfaz en su navegador...
+start "" cmd /c "timeout /t 5 >nul & start http://localhost:10005"
+
 call uv run python server.py
 
 echo.
